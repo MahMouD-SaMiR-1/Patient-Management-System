5b9291490dea0e6036458f68e2f8e7a762f2fe08
Mahmoud Samir <ms5698450@gmail.com>
2025-02-12 13:37:32 +0200

    create appointment done

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,12 +1,16 @@
-import PatientForm from "@/components/forms/PatientForm";
+import AppointmentForm from "@/components/forms/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.actions";
 import Image from "next/image";
-import Link from "next/link";
 
-export default function NewAppointment() {
+export default async function NewAppointment({
+	params: { userId },
+}: SearchParamProps) {
+	const patient = await getPatient(userId);
+
 	return (
 		<div className="flex h-screen max-h-screen">
-			<section className="remove-scrollbar container my-auto">
-				<div className="sub-container max-w-[496px]">
+			<section className="remove-scrollbar container">
+				<div className="sub-container max-w-[860px] flex-1 justify-between">
 					<Image
 						src="/assets/icons/logo-full.svg"
 						height={1000}
@@ -15,24 +19,22 @@ export default function NewAppointment() {
 						className="mb-12 h-10 w-fit "
 					/>
 
-					<PatientForm />
+					<AppointmentForm
+						type="create"
+						userId={userId}
+						patientId={patient.$id}
+					/>
 
-					<div className="text-14-regular mt-20 flex justify-between">
-						<p className="justify-items-end text-dark-600 xl:text-left">
-							© 2025CarePulse
-						</p>
-						<Link href="/?admin=true" className="text-green-500">
-							Admin
-						</Link>
-					</div>
+					<p className="copyright mt-10 py-12">© 2025CarePulse</p>
 				</div>
 			</section>
+
 			<Image
-				src="/assets/images/onboarding-img.png"
+				src="/assets/images/appointment-img.png"
 				height={1000}
 				width={1000}
-				alt="patient"
-				className="side-img max-w-[50%]"
+				alt="appointment"
+				className="side-img max-w-[390px] bg-bottom"
 			/>
 		</div>
 	);
